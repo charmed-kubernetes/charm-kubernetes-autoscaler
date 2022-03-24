fcd2c9fb2f87a313e9f5d3895571cfa2a3e22294
Adam Dyess <adam.dyess@canonical.com>
2022-03-23 21:36:06 -0500

    simply the juju-scale config to only accept yaml, not colon separated fields

--- tests/unit/test_config_scale.py
+++ tests/unit/test_config_scale.py
@@ -8,34 +8,35 @@ def test_default_scale():
     assert ConfigScale("").nodes(None) == []
 
 
-def test_comma_separated_string():
-    scale = ConfigScale("3:5:kubernetes-worker,0:10:kubernetes-worker-gpu,")
+def test_json_list():
+    scale = ConfigScale(
+        "["
+        '{"min": 3, "max":5, "application": "kubernetes-worker"},'
+        '{"min": 0, "max":10, "application": "kubernetes-worker-gpu"}'
+        "]"
+    )
     assert scale.nodes("test") == ["3:5:test:kubernetes-worker", "0:10:test:kubernetes-worker-gpu"]
 
 
-def test_list_of_strings_yaml():
-    scale = ConfigScale("---\n" "- 3:5:kubernetes-worker\n" "- 0:10:kubernetes-worker-gpu\n")
-    assert scale.nodes("test") == ["3:5:test:kubernetes-worker", "0:10:test:kubernetes-worker-gpu"]
-
-
-def test_list_of_mappings_and_string_yaml():
+def test_list_of_mappings_and_json_yaml():
     scale = ConfigScale(
         "---\n"
         "- min: 3\n"
         "  max: 5\n"
         "  application: kubernetes-worker\n"
-        "- 0:10:kubernetes-worker-gpu\n"
+        '- {"min": 0, "max":10, "application": "kubernetes-worker-gpu"}\n'
     )
     assert scale.nodes("test") == ["3:5:test:kubernetes-worker", "0:10:test:kubernetes-worker-gpu"]
 
 
-def test_list_of_strings_json():
-    scale = ConfigScale('["3:5:kubernetes-worker","0:10:kubernetes-worker-gpu",]')
-    assert scale.nodes("test") == ["3:5:test:kubernetes-worker", "0:10:test:kubernetes-worker-gpu"]
-
-
 def test_yaml_with_model_part():
-    scale = ConfigScale("- 0:10:cdcaed9f-336d-47d3-83ba-d9ea9047b18c:kubernetes-worker-gpu")
+    scale = ConfigScale(
+        "---\n"
+        "- min: 0\n"
+        "  max: 10\n"
+        "  model: cdcaed9f-336d-47d3-83ba-d9ea9047b18c\n"
+        "  application: kubernetes-worker-gpu\n"
+    )
     assert scale.nodes(None) == ["0:10:cdcaed9f-336d-47d3-83ba-d9ea9047b18c:kubernetes-worker-gpu"]
 
 
@@ -57,52 +58,39 @@ def test_error_invalid_yaml_type():
     assert str(ie.value) == "juju_scale invalid: Unexpected yaml collection type"
 
 
-def test_error_missing_element():
+@pytest.mark.parametrize(
+    "cfg, missing_element",
+    [
+        ("{max: 0, application: kubernetes-worker}", "min"),
+        ("{min: 0, application: kubernetes-worker}", "max"),
+        ("{min: 0, max: 5}", "application"),
+    ],
+)
+def test_error_missing_element(cfg, missing_element):
     with pytest.raises(ConfigError) as ie:
-        ConfigScale("---\n" "- min: 3\n" "  max: 5\n")
-    assert str(ie.value) == "juju_scale invalid: missing required element 'application'"
+        ConfigScale(f"- {cfg}")
+    assert str(ie.value).startswith(
+        f"juju_scale invalid: missing required element '{missing_element}'"
+    )
 
 
 def test_error_max_gt_min():
     with pytest.raises(ConfigError) as ie:
-        ConfigScale("5:3:kubernetes-worker,")
-    assert (
-        str(ie.value)
-        == "juju_scale invalid: <min> should be less than <max> '5:3:kubernetes-worker'"
-    )
+        ConfigScale("- {min: 5, max: 3, application: kubernetes-worker}")
+    assert str(ie.value).startswith("juju_scale invalid: <min> should be less than <max> -")
 
 
 def test_error_min_not_an_int():
-    cfg = "X:3:kubernetes-worker"
+    cfg = "- {min: X, max: 3, application: kubernetes-worker}"
     with pytest.raises(ConfigError) as ie:
         ConfigScale(cfg)
-    assert (
-        str(ie.value) == f"juju_scale invalid: <min> & <max> must be non-negative integers '{cfg}'"
+    assert str(ie.value).startswith(
+        "juju_scale invalid: <min> & <max> must be non-negative integers -"
     )
 
 
-def test_error_not_enough_parts():
-    cfg = "0:kubernetes-worker"
+def test_error_model_invalid():
+    cfg = "- {min: 0, max: 1, model: 2, application: kubernetes-worker}"
     with pytest.raises(ConfigError) as ie:
         ConfigScale(cfg)
-    assert (
-        str(ie.value)
-        == f"juju_scale invalid: Must contain at least 3 parts <min>:<max>:<application> '{cfg}'"
-    )
-
-
-def test_error_too_many_parts():
-    cfg = "0:1:2:3:kubernetes-worker"
-    with pytest.raises(ConfigError) as ie:
-        ConfigScale(cfg)
-    assert (
-        str(ie.value)
-        == f"juju_scale invalid: Must contain 4 parts <min>:<max>:<model>:<application> '{cfg}'"
-    )
-
-
-def test_error_model_invalid_part():
-    cfg = "0:1:2:kubernetes-worker"
-    with pytest.raises(ConfigError) as ie:
-        ConfigScale(cfg)
-    assert str(ie.value) == f"juju_scale invalid: Invalid model part in '{cfg}'"
+    assert str(ie.value).startswith("juju_scale invalid: Invalid model uuid - ")
